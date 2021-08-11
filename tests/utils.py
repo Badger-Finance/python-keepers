caf313cb85e6c33f14fc13f00c4ba1686d84c848
Ayush Shukla <shuklaayush247@gmail.com>
2021-08-12 03:25:50 +0530

    fix tests

--- tests/utils.py
+++ tests/utils.py
@@ -5,13 +5,6 @@ test_address = "0xD88a9aF149366d57aEbc32D2eABAdf93EdA41A84"
 test_key = "0f0bdc830bde4be43c3a54c369c6f6a94ac9071911dc3913e35ce5ed8fe955b9"
 
 
-def get_abi(contract_id: str, network: str) -> dict:
-    if network not in ["bsc", "eth"]:
-        raise ValueError(f"Network: '{network}' not found")
-    with open(f"./abi/{network}/{contract_id}.json") as f:
-        return json.load(f)
-
-
 def get_strategy(
     strategy_address: str, network: str, abi_file: str = "strategy"
 ) -> Contract:
