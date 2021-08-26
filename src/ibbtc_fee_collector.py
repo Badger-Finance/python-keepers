5d3b91ce38ae4ba69bf0558fe7edb421edf0d52d
brookstaylor <brooks.o.taylor@vanderbilt.edu>
2021-08-26 14:01:02 -0400

    black formatting

--- src/ibbtc_fee_collector.py
+++ src/ibbtc_fee_collector.py
@@ -104,7 +104,9 @@ class ibBTCFeeCollector:
             tx_hash = self.__send_collection_tx()
             succeeded, _ = confirm_transaction(self.web3, tx_hash)
             if succeeded:
-                gas_price_of_tx = get_gas_price_of_tx(self.web3, self.eth_usd_oracle, tx_hash)
+                gas_price_of_tx = get_gas_price_of_tx(
+                    self.web3, self.eth_usd_oracle, tx_hash
+                )
                 send_success_to_discord(
                     tx_hash=tx_hash,
                     tx_type="ibBTC Fee Collection",
