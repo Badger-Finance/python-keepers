92ede477ac66726d11206ee3dd1a4ef450a9c8bc
btcookies <31353111+btcookies@users.noreply.github.com>
2021-11-02 11:29:09 -0400

    Bug: fix eth harvest cadence so only harvesting every 4/5 days (#107)
    * fix harvest cadence so harvesting every 5 days
    
    * set min harvest time to 4 days
    
    * fix comment accuracy
    
    * refactor block offset into utils function, only use max_time_between_harvests
    
    * remove unused import
    
    * chore: fix typo, remove imports
    
    * anotha typo
    
    * align comments with cadence variables

--- tests/test_general_harvester.py
+++ tests/test_general_harvester.py
@@ -7,7 +7,13 @@ from web3 import contract
 
 from config.constants import MULTICHAIN_CONFIG
 from src.general_harvester import GeneralHarvester
-from src.utils import get_abi, get_last_harvest_times, hours, get_secret
+from src.utils import (
+    get_abi,
+    get_last_harvest_times,
+    hours,
+    get_secret,
+    seconds_to_blocks,
+)
 from tests.utils import test_address, test_key
 
 ETH_USD_CHAINLINK = web3.toChecksumAddress(MULTICHAIN_CONFIG["eth"]["gas_oracle"])
@@ -212,7 +218,7 @@ def test_is_time_to_harvest(web3, chain, keeper_address, harvester, strategy):
     accounts[0].transfer(keeper_address, "10 ether")
 
     # Strategy should be harvestable at this point
-    chain.sleep(hours(72))
+    chain.sleep(hours(121))
     chain.mine(1)
     assert harvester.is_time_to_harvest(strategy) == True
     harvester.harvest(strategy)
@@ -220,8 +226,11 @@ def test_is_time_to_harvest(web3, chain, keeper_address, harvester, strategy):
     # Strategy shouldn't be harvestable
     assert harvester.is_time_to_harvest(strategy) == False
 
-    # Strategy should be harvestable again after 72 hours
+    # Should only be able to harvest after 120 hours
     chain.sleep(hours(72))
+    chain.mine(1)
+    assert harvester.is_time_to_harvest(strategy) == False
+    chain.sleep(hours(49))
     chain.mine(1)
     assert harvester.is_time_to_harvest(strategy) == True
     harvester.harvest(strategy)
@@ -229,22 +238,44 @@ def test_is_time_to_harvest(web3, chain, keeper_address, harvester, strategy):
 
 @pytest.mark.require_network("hardhat-fork")
 def test_is_time_to_harvest_rewards_manager(
-    web3, chain, keeper_address, harvester, rewards_manager_strategy
+    web3,
+    chain,
+    keeper_address,
+    harvester,
+    rewards_manager_strategy,
+    setup_rewards_manager,
 ):
+    harvester.keeper_acl = harvester.web3.eth.contract(
+        address=harvester.web3.toChecksumAddress(
+            MULTICHAIN_CONFIG["eth"]["rewards_manager"]
+        ),
+        abi=get_abi("eth", "rewards_manager"),
+    )
+    harvester.last_harvest_times = get_last_harvest_times(
+        harvester.web3,
+        harvester.keeper_acl,
+        start_block=harvester.web3.eth.block_number - seconds_to_blocks(hours(120)),
+        etherscan_key=os.getenv("ETHERSCAN_TOKEN"),
+    )
     strategy_name = rewards_manager_strategy.functions.getName().call()
     accounts[0].transfer(keeper_address, "10 ether")
 
     # Strategy should be harvestable at this point
-    chain.sleep(hours(72))
+    chain.sleep(hours(121))
     chain.mine(1)
     assert harvester.is_time_to_harvest(rewards_manager_strategy) == True
     harvester.harvest_rewards_manager(rewards_manager_strategy)
 
+    assert harvester.last_harvest_times[rewards_manager_strategy.address]
+
     # Strategy shouldn't be harvestable
     assert harvester.is_time_to_harvest(rewards_manager_strategy) == False
 
-    # Strategy should be harvestable again after 72 hours
+    # Should only be able to harvest after 120 hours
     chain.sleep(hours(72))
+    chain.mine(1)
+    assert harvester.is_time_to_harvest(rewards_manager_strategy) == False
+    chain.sleep(hours(49))
     chain.mine(1)
     assert harvester.is_time_to_harvest(rewards_manager_strategy) == True
     harvester.harvest_rewards_manager(rewards_manager_strategy)
