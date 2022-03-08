02f29c8af8629b5058d2156f96d287cba6458a0c
Andrew Kulikov <jadecoresky@gmail.com>
2022-03-08 20:32:37 +0200

    chore: fix binary secret retrieval and add tests

--- src/utils.py
+++ src/utils.py
@@ -74,11 +74,13 @@ def get_secret(
         if "SecretString" in get_secret_value_response:
             return json.loads(get_secret_value_response["SecretString"]).get(secret_key)
         else:
-            return base64.b64decode(get_secret_value_response["SecretBinary"]).get(
+            return json.loads(base64.b64decode(
+                get_secret_value_response["SecretBinary"]
+            ).decode(
+                "utf-8"
+            )).get(
                 secret_key
             )
-
-    return None
 
 
 # TODO: Don't duplicate common abis for all chains
--- tests/test_secrets_utils.py
+++ tests/test_secrets_utils.py
@@ -1,4 +1,8 @@
+import base64
 from unittest.mock import MagicMock
+
+import pytest
+from botocore.exceptions import ClientError
 
 from src.utils import get_secret
 
@@ -18,3 +22,42 @@ def test_get_secret_happy(mocker):
         )
     )
     assert get_secret("some_secret_name", "some_key") == "secret_value"
+
+
+def test_get_secret_happy_binary(mocker):
+    binary_secret_string = '{"some_key": "secret_value"}'
+    string_bytes = binary_secret_string.encode("ascii")
+    mocker.patch(
+        "src.utils.boto3.session.Session",
+        return_value=MagicMock(
+            client=MagicMock(
+                return_value=MagicMock(
+                    get_secret_value=MagicMock(return_value={
+                        "SecretBinary": base64.b64encode(string_bytes),
+                    })
+                )
+            )
+        )
+    )
+    assert get_secret("some_secret_name", "some_key") == "secret_value"
+
+
+def test_get_secret_client_raises(mocker):
+    mocker.patch(
+        "src.utils.boto3.session.Session",
+        return_value=MagicMock(
+            client=MagicMock(
+                return_value=MagicMock(
+                    get_secret_value=MagicMock(
+                        side_effect=ClientError(
+                            {'Error': {
+                                'Code': "DecryptionFailureException"
+                            }}, ''
+                        )
+                    )
+                )
+            )
+        )
+    )
+    with pytest.raises(ClientError):
+        get_secret("some_secret_name", "some_key")
