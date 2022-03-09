285018e622e3fedb0de8beace9a7842251986d75
Andrew Kulikov <jadecoresky@gmail.com>
2022-03-09 14:10:52 +0200

    chore: add discord utils units

--- tests/test_discord_utils.py
+++ tests/test_discord_utils.py
@@ -1,9 +1,13 @@
 from hexbytes import HexBytes
 
 from src.utils import send_error_to_discord
+from src.utils import send_oracle_error_to_discord
+from src.utils import send_rebase_error_to_discord
+from src.utils import send_rebase_to_discord
+from src.utils import send_success_to_discord
 
 
-def test_send_error_to_discord(mocker):
+def test_send_error_to_discord_send_called(mocker):
     mocker.patch("src.utils.get_secret")
     mocker.patch("src.utils.RequestsWebhookAdapter")
     webhook = mocker.patch("src.utils.Webhook.from_url")
@@ -12,4 +16,43 @@ def test_send_error_to_discord(mocker):
         tx_type="whatever",
         tx_hash=HexBytes("0x123123"),
     )
+    assert webhook.return_value.send.called
+
+
+def test_send_success_to_discord_send_called(mocker):
+    mocker.patch("src.utils.get_secret")
+    mocker.patch("src.utils.RequestsWebhookAdapter")
+    webhook = mocker.patch("src.utils.Webhook.from_url")
+
+    send_success_to_discord(
+        tx_hash=HexBytes("0x123123"),
+        tx_type="whatever",
+    )
+    assert webhook.return_value.send.called
+
+
+def test_send_rebase_to_discord_send_called(mocker):
+    mocker.patch("src.utils.get_secret")
+    mocker.patch("src.utils.RequestsWebhookAdapter")
+    webhook = mocker.patch("src.utils.Webhook.from_url")
+
+    send_rebase_to_discord(tx_hash=HexBytes("0x123123"),)
+    assert webhook.return_value.send.called
+
+
+def test_send_rebase_error_to_discord_send_called(mocker):
+    mocker.patch("src.utils.get_secret")
+    mocker.patch("src.utils.RequestsWebhookAdapter")
+    webhook = mocker.patch("src.utils.Webhook.from_url")
+
+    send_rebase_error_to_discord(Exception())
+    assert webhook.return_value.send.called
+
+
+def test_send_oracle_error_to_discord_send_called(mocker):
+    mocker.patch("src.utils.get_secret")
+    mocker.patch("src.utils.RequestsWebhookAdapter")
+    webhook = mocker.patch("src.utils.Webhook.from_url")
+
+    send_oracle_error_to_discord(tx_type="whatever",error=Exception())
     assert webhook.return_value.send.called
