dd36ce4420cff7713f4af31e845ef2bbc8cd1086
brookstaylor <brooks.o.taylor@vanderbilt.edu>
2022-03-15 17:01:52 -0400

    chore: add non happy path tests

--- tests/test_vester.py
+++ tests/test_vester.py
@@ -34,14 +34,29 @@ def mock_arb_vester():
     return vester
 
 
-def test_vest(mock_arb_vester, mocker):
+def test_vest_happy(mock_arb_vester, mocker):
     success_message = mocker.patch("src.vester.send_success_to_discord")
     confirm_transaction = mocker.patch(
         "src.vester.confirm_transaction", return_value=(True, True)
     )
-    mocker.patch(
-        "src.vester.get_gas_price_of_tx", return_value=1
+    mocker.patch("src.vester.get_gas_price_of_tx", return_value=1)
+    mock_arb_vester.vest()
+    assert confirm_transaction.called
+    assert success_message.called
+
+
+def test_vest_pending_tx(mock_arb_vester, mocker):
+    success_message = mocker.patch("src.vester.send_success_to_discord")
+    confirm_transaction = mocker.patch(
+        "src.vester.confirm_transaction", return_value=(False, False)
     )
     mock_arb_vester.vest()
     assert confirm_transaction.called
     assert success_message.called
+
+
+def test_vest_error(mock_arb_vester, mocker):
+    mock_arb_vester.__send_vest_tx = MagicMock(side_effect=Exception)
+    error_message = mocker.patch("src.vester.send_error_to_discord")
+    mock_arb_vester.vest()
+    assert error_message.called
