6c9ddf9218a177bb9c87cc24c374c01b0a2b19f4
Andrew Kulikov <jadecoresky@gmail.com>
2022-03-16 12:59:43 +0200

    chore: fix testing mocks

--- tests/test_vester.py
+++ tests/test_vester.py
@@ -17,24 +17,22 @@ def mock_arb_vester():
         base_oracle_address=ARB_ETH_USD_CHAINLINK,
         node_url="dummynode.com",
     )
-    vester.web3 = (
-        MagicMock(
-            eth=MagicMock(
-                wait_for_transaction_receipt=MagicMock(return_value={}),
-                get_transaction_count=MagicMock(return_value={}),
-                max_priority_fee=MagicMock(return_value={}),
-                account=MagicMock(return_value={}),
-                send_raw_transaction=MagicMock(return_value={}),
-                gas_price=MagicMock(return_value={}),
-            )
-        ),
+    vester.web3 = MagicMock(
+        eth=MagicMock(
+            wait_for_transaction_receipt=MagicMock(return_value={}),
+            get_transaction_count=MagicMock(return_value={}),
+            max_priority_fee=MagicMock(return_value={}),
+            account=MagicMock(return_value={}),
+            send_raw_transaction=MagicMock(return_value={}),
+            gas_price=MagicMock(return_value={}),
+        )
     )
     vester.vesting_contract = MagicMock(
         functions=MagicMock(
             release=MagicMock(buildTransaction=MagicMock(return_value={}))
         )
     )
-    return vester
+    yield vester
 
 
 def test_vest_happy(mock_arb_vester, mocker):
